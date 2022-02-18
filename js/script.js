496b9132ac6e3aefb501b1ae251ed2b254c0629b
markizlaki <pogorelovmaxim@gmail.com>
2022-02-18 15:30:38 +0200

    last change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+// бургер меню
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation_active')
+})
+
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active')
+});
+
+// музыка
+
+try {
+const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3')
+
+const checkMute = () => {
+    if (mute.checked) {
+        audio.play().catch(() => {
+            mute.checked = false
+        });
+    } else {
+        audio.pause();
+    }
+};
+
+if (mute) {
+    setTimeout(checkMute, 2000)
+}
+
+mute.addEventListener('change', checkMute)
+} catch {
+    console.log('на этой странице нет музыки');
+}
+
+try {
 const sliderThumbs = new Swiper('.slider-thumbs', {
     loop: true,
     spaceBetween: 20,
@@ -17,3 +56,22 @@ const sliderMain = new Swiper('.slider-main', {
 
 sliderThumbs.controller.control = sliderMain;
 sliderMain.controller.control = sliderThumbs;
+
+const videos = document.querySelectorAll('video');
+
+sliderMain.on('slideChange', () => {
+    for (let i = 0; i < videos.length; i += 1) {
+        videos[i].pause();
+    }
+})
+
+const pagination = document.querySelector('.pagination');
+const paginationButton = document.querySelector('.pagination__arrow');
+
+paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination_active')
+}) 
+
+} catch {
+    console.log('на этой странице нет слайдера');
+}
